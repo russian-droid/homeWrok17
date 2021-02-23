275f2809223803bc4bf01c256efcd066aeca6f19
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-22 23:34:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-def printer_error(s):
-    #length of the string
-    s_lenght=len(s)
-    error_length = 0
+'''
+The new "Avengers" movie has just been released! There are a lot of people at the cinema box
+office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. 
+An "Avengers" ticket costs 25 dollars.
+Vasya is currently working as a clerk. He wants to sell a ticket to every single person in 
+this line.
+Can Vasya sell a ticket to every person and give change if he initially has no money and
+sells the tickets strictly in the order people queue?
+Return YES, if Vasya can sell a ticket to every person and give change with the bills he
+has at hand at that moment. Otherwise return NO.
 
-    for x in s:
-        #sort the string by looking for anything passed M
-        if x not in "abcdefghijklm":
-            #count errors
-            error_length = error_length + 1
-        else: 
-            pass
-    #create a variable to return it, so we can print it outside of the function
-    msg = '{}/{}'.format(error_length,s_lenght)
-    return msg
-    #OR just
-    #return error_length + "/" + s_lenght
+tickets([25, 25, 50]) # => YES 
+tickets([25, 100]) # => NO. Vasya will not have enough money to give change to 100 dollars
+tickets([25, 25, 50, 50, 100]) # => NO. Vasya will not have the right bills to give 75 
+dollars of change (you can't make two bills of 25 from one of 50)
+'''
+
+
+money[25, 25, 50]
+money[25, 100]
+money[25, 25, 50, 50, 100]
+
+ticket = 25
+#start checking every element minus ticket price ($25)
+change = money[2] - ticket
+if change == 0:
+    print('YES')
+else:
+    for x in money:
+        
+        
+#start checking if we can give change from existing bills 
+if change - money[1] - money[0] == 0:
+    print('YES')
+else:
+    print('NO')
