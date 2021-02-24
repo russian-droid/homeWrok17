a950821b461ea83d36b12edeb92f22de003e34e4
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-23 23:19:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 '''
+codeWars kata from codewars.com
+
 The new "Avengers" movie has just been released! There are a lot of people at the cinema box
 office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. 
 An "Avengers" ticket costs 25 dollars.
@@ -15,22 +17,27 @@ tickets([25, 25, 50, 50, 100]) # => NO. Vasya will not have the right bills to g
 dollars of change (you can't make two bills of 25 from one of 50)
 '''
 
-
-money[25, 25, 50]
-money[25, 100]
-money[25, 25, 50, 50, 100]
-
-ticket = 25
-#start checking every element minus ticket price ($25)
-change = money[2] - ticket
-if change == 0:
-    print('YES')
-else:
-    for x in money:
-        
-        
-#start checking if we can give change from existing bills 
-if change - money[1] - money[0] == 0:
-    print('YES')
-else:
-    print('NO')
+def tickets(people):
+    d25 = 0
+    d50 = 0
+    d100 = 0
+    for i in people:
+        if i == 25:
+            d25+=1
+        elif i == 50:
+            if d25 >= 1:
+                d50+=1
+                d25-=1
+            else:
+                return 'NO'
+        else:
+            if (d50 >=1 and d25 >=1):
+                d100+=1
+                d50-=1
+                d25-=1
+            elif d25 >=3:
+                d100+=1
+                d25-=3
+            else:
+                return 'NO'
+    return 'YES'
