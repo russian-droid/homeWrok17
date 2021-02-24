e95dd8bba61d5e9c0d30c3138b62572052dce184
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-23 23:36:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -61,3 +61,15 @@ def tickets(people):
             if (paid > ticketPrice) and (changeGiven == False):
                     return "NO";
     return "YES";
+
+#---------------------
+def tickets(a):
+    n25 = n50 = n100 = 0
+    for e in a:
+        if   e==25            : n25+=1
+        elif e==50            : n25-=1; n50+=1
+        elif e==100 and n50>0 : n25-=1; n50-=1
+        elif e==100 and n50==0: n25-=3
+        if n25<0 or n50<0:
+            return 'NO'
+    return 'YES'
